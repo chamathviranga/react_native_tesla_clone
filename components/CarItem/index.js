5c56341e75681448a84c7f5cdf1c5496bf993db1
chamath viranga <chamathviranga@gmail.com>
2022-03-08 02:23:01 +0530

    button componen : done

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {Text, View, ImageBackground } from 'react-native';
 import styles from './style';
 
+import StyledButton from '../StyledButton';
+
 const CarItem = (props) => {
   return (
       
@@ -16,6 +18,22 @@ const CarItem = (props) => {
           <Text style={styles.title}>Model S</Text>
           <Text style={styles.subtitle}>Starting at $69,420</Text>
         </View>
+
+        <StyledButton 
+          type='primary'
+          content = {'Custom Order'}
+          onPress={()=>{
+              console.warn('Custom Order was pressed ..');
+          }}
+        />
+
+        <StyledButton 
+          type='secondary'
+          content = {'Existing Inventory'}
+          onPress={()=>{
+              console.warn('Existing Inventory was pressed ..');
+          }}
+        />
 
       </View>
 
